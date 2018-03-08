3a4ae22547905b65acbd92a6ae945c4e55f783ac
Leonardo <leonardo.crecente@gmail.com>
2018-03-08 01:06:33 -0300

    1 tab identation to 2 spaces

--- js/simple-mobile-menu.js
+++ js/simple-mobile-menu.js
@@ -1,20 +1,20 @@
 ;(function( window, document, $, undefined ) {
-	"use strict";
+  "use strict";
 
-	// Menu Toggle
-	(function() {
-		$(function() {
+  // Menu Toggle
+  (function() {
+    $(function() {
 
-			var $toggle = $(".js-smm-toggle");
-			var $container = $(".js-smm-container");
+      var $toggle = $(".js-smm-toggle");
+      var $container = $(".js-smm-container");
 
-			if( ! $toggle.length || ! $container.length ) return;
+      if( ! $toggle.length || ! $container.length ) return;
 
-			$toggle.click(function() {
-				$toggle.toggleClass("smm__toggle--open");
-				$container.toggleClass("smm__container--open");
-			});
-		});
-	})();
+      $toggle.click(function() {
+        $toggle.toggleClass("smm__toggle--open");
+        $container.toggleClass("smm__container--open");
+      });
+    });
+  })();
 
 })( window, document, jQuery );
